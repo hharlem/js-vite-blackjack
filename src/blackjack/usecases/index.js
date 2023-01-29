fbd7d6aa87aa280719b9e73682874503534ffc3f
Harlem <hans.uriel1234@gmail.com>
2023-01-28 19:42:45 -0600

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+// Archivo de barril 
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
+export {crearCartaHTML} from './crear-carta-html'
